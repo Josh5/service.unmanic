c5ca2835452ae3f18864e604c35beefc47939bea
Josh.5 <jsunnex@gmail.com>
2021-05-24 16:38:19 +1200

    Update header license to match SPDX

---
+++ .idea/fileTemplates/internal/Python Script.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+    ${PROJECT_NAME}.${FILE_NAME}
+
+    Written by:               Josh.5 <jsunnex@gmail.com>
+    Date:                     ${DAY} ${MONTH_NAME_SHORT} ${YEAR}, (${TIME})
+
+    Copyright:
+        Copyright (C) ${YEAR} Josh Sunnex
+
+        This program is free software: you can redistribute it and/or modify it under the terms of the GNU General 
+        Public License as published by the Free Software Foundation, version 3.
+        
+        This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the 
+        implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+        for more details.
+        
+        You should have received a copy of the GNU General Public License along with this program. 
+        If not, see <https://www.gnu.org/licenses/>.
+
+"""
